d128ec92ef8f7f40a55aee706909bae4226b402a
Esteban SEMELLIER <esteban.semellier@mobiapps.fr>
2024-05-28 16:53:48 +0200

    rename iOS app refacto code (Extracted Views)

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutflix/ui/widgets/movies_slider.dart';
+import 'package:flutflix/ui/widgets/trending_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -33,30 +35,26 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Text('Trending Movies', style: GoogleFonts.aBeeZee(fontSize: 25)
                 ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  width: double.infinity,
-                  child: CarouselSlider.builder(itemCount: 10,
-                    options: CarouselOptions(
-                      height: 300, autoPlay: true,
-                      viewportFraction: 0.55,
-                      enlargeCenterPage: true,
-                      pageSnapping: true,
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                      autoPlayAnimationDuration: const Duration(seconds: 2),
-                    ),
-                    itemBuilder: (context, itemIndex, pageViewIndex){
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Container(
-                          height: 300,
-                          width: 200,
-                          color: Colors.amber,
-                        ),
-                      );
-                    },
+                const SizedBox(height: 32),
+                const TrendingSlider(),
+                const SizedBox(height: 32),
+                Text(
+                  "Top rated movies",
+                  style: GoogleFonts.aBeeZee(
+                    fontSize: 25
                   ),
-                )
+                ),
+                const SizedBox(height: 32),
+                const MoviesSlider(),
+                const SizedBox(height: 32),
+                Text(
+                  "Upcoming movies",
+                  style: GoogleFonts.aBeeZee(
+                      fontSize: 25
+                  ),
+                ),
+                const SizedBox(height: 32),
+                const MoviesSlider()
               ],
             ),
           ),
